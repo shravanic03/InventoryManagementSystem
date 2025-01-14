5e5b0525a77617976035d36011a16138dc6d901f
Shravani Chougule <shraa1603@gmail.com>
2025-01-14 13:06:38 +0530

    blah blah blah

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-800 bg-black text-5xl'> Inventory Management System </h1>
+        
+    </>
+  )
+}
+
+export default App
